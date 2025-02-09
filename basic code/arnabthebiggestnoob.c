d75122ea71491414aedb6831fc7b8f4d8d14f367
DevBhargab <anweshabhargab2024@outlook.com>
2025-02-09 13:31:21 +0530

    loop 1

---
+++ basic code/arnabthebiggestnoob.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    for(int i=0; i<=4;i++){
+        printf("%d\n",i);
+    }
+    // while loops
+    printf("while loop\n");
+     int i =0;
+        while(i<=5){
+            printf("%d\n",i);
+            i++;
+        }
+    return 0;
+}
