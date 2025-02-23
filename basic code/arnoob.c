0306200292dd37b534448f050dab42d9ebb7f885
DevBhargab <anweshabhargab2024@outlook.com>
2025-02-23 13:58:32 +0530

    pointer

---
+++ basic code/arnoob.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void swap (int *a , int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main (){
+    int a = 5;
+    int b = 10;
+    swap(&a,&b);
+   printf("a = %d\nb = %d\n",a,b);
+} 
