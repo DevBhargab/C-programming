7ca26cdde31c569a18634b76f8ee70947a8bc5f5
DevBhargab <anweshabhargab2024@outlook.com>
2025-03-02 14:00:15 +0530

    exan.1111

--- basic code/advgrfdfd.c
+++ basic code/advgrfdfd.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
-int main()
-{
-    float r;
-    printf("Enter the  of the r\n");
-    scanf("%f", &r);
-    float pi = 3.14;
-    printf("%f\n", r * r * pi);
+
+int main() {
+    int gadha ;
+    printf("enter a gadha number\n");
+     scanf("%d",&gadha);
+    switch (gadha){
+     case 1:   
+    printf("your gadha is :triparna");
+    break;
+    case 2:
+    printf("your gadha is :adrita ");
+    break;
+    case 3:
+    printf("babai , not a gadha");
+    break;
+    default:4;
+    printf("there is no gadha");
+    break;
+    }
+    //int 1=mon;
+    //int 2=bunu;
+    //int 3=bhargab
+    
+
     return 0;
 }
+
